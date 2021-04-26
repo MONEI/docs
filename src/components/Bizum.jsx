633b1124b27360d30bd2ce470830c4cbf645eb9b
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2021-04-26 19:09:41 +0200

    fix build

--- src/components/Bizum.jsx
+++ src/components/Bizum.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as monei from '@monei-js/components';
+if (typeof window !== 'undefined') {
+  const React = require('react');
+  const ReactDOM = require('react-dom');
+  const monei = require('@monei-js/components');
 
-const BizumComponent = monei.Bizum.driver('react', {
-  React: React,
-  ReactDOM: ReactDOM
-});
+  const BizumComponent = monei.Bizum.driver('react', {
+    React: React,
+    ReactDOM: ReactDOM
+  });
 
-export const Bizum = React.memo(() => {
-  return (
-    <BizumComponent
-      accountId="2975bcfa-7bbc-422d-af48-c66759d87b69"
-      amount={100}
-      currency="EUR"
-      onSubmit={(result) => {
-        console.log(result);
-      }}
-      onError={(error) => {
-        console.log(error);
-      }}
-    />
-  );
-});
+  module.exports.Bizum = () => {
+    return (
+      <BizumComponent
+        accountId="2975bcfa-7bbc-422d-af48-c66759d87b69"
+        amount={100}
+        currency="EUR"
+        onSubmit={(result) => {
+          console.log(result);
+        }}
+        onError={(error) => {
+          console.log(error);
+        }}
+      />
+    );
+  };
+} else {
+  module.exports.Bizum = () => null;
+}
