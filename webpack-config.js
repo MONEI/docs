6d4f3f767a0ac31eb359b15ff4ecf6a86a5e82d1
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2021-07-01 13:12:54 +0200

    - upgrade dependencies - fix trailing slashes - fix urls

--- webpack-config.js
+++ webpack-config.js
@@ -1,15 +1,33 @@
 const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = function (context, options) {
   const isProd = process.env.NODE_ENV === 'production';
   return {
     name: 'webpack-config',
     configureWebpack(config, isServer, utils) {
-      if (!isServer) return {};
       return {
+        resolve: {
+          fallback: {
+            path: require.resolve('path-browserify'),
+            http: false,
+            fs: false,
+            os: false
+          }
+        },
+        module: {
+          rules: [
+            {
+              test: /\.js/,
+              include: /node_modules\/yaml\/browser/,
+              type: 'javascript/auto'
+            }
+          ]
+        },
         plugins: [
-          new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer']
-          })
+          new webpack.DefinePlugin({
+            'process.versions.node': JSON.stringify(process.versions.node || '0.0.0')
+          }),
+          new NodePolyfillPlugin()
         ]
       };
     }
