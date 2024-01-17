2705cd744de60bb50d3248b9c8cfcacaabfdbd9c
Joan Sanfeliu <124098537+joan-sanfeliu@users.noreply.github.com>
2024-01-17 14:30:58 +0100

    feat: first version of cm token docs

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
         'integrations/use-payment-modal',
         'integrations/build-custom-checkout',
         'integrations/pay-by-link',
-        'integrations/use-qr-payments',
+        'integrations/use-qr-payments'
       ],
       'E-commerce platforms': [
         'e-commerce/shopify',
@@ -16,7 +16,8 @@ module.exports = {
         'e-commerce/woocommerce',
         'e-commerce/salesforce',
         'e-commerce/adobe-commerce',
-        'e-commerce/prestashop'
+        'e-commerce/prestashop',
+        'e-commerce/channel-tokenization'
       ],
       'Payment methods': [
         'payment-methods/card',
