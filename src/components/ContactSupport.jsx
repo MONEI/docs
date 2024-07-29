e008aab5178a73a2d67fd68860521d96b50b0b5f
Dmitriy <dn@monei.com>
2024-07-29 18:41:54 +0200

    fix ContactSupport link

--- src/components/ContactSupport.jsx
+++ src/components/ContactSupport.jsx
@@ -6,59 +6,7 @@ export const ContactSupport = ({ children }) => {
     if (!zE) return;
     e.preventDefault();
     try {
-      zE("webWidget", "updateSettings", {
-        webWidget: {
-          chat: {
-            suppress: true
-          },
-          helpCenter: {
-            suppress: true
-          },
-          talk: {
-            suppress: true
-          },
-          answerBot: {
-            suppress: true
-          },
-          contactForm: {
-            suppress: false,
-            ticketForms: [
-              {
-                id: 360000322338,
-                title: false
-              }
-            ]
-          }
-        }
-      });
-      zE.activate();
-      zE("webWidget:on", "close", () => {
-        zE("webWidget", "updateSettings", {
-          webWidget: {
-            chat: {
-              suppress: false
-            },
-            helpCenter: {
-              suppress: false
-            },
-            talk: {
-              suppress: false
-            },
-            answerBot: {
-              suppress: false
-            },
-            contactForm: {
-              suppress: false,
-              ticketForms: [
-                {
-                  id: 360000322338,
-                  title: false
-                }
-              ]
-            }
-          }
-        });
-      });
+      zE("messenger", "open");
     } catch (error) {
       console.error(error);
       window.location.href = "https://support.monei.com/hc/requests/new";
